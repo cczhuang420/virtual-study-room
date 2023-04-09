1b0cf3197fc1086fcbc98e36897ce6adceac12e8
qingyang0506 <1565134797qq@gmail.com>
2023-04-09 20:59:17 +1200

    add some icons

---
+++ client/src/components/RoomCard.jsx
@@ -0,0 +1,82 @@
+import { Box, Typography } from "@mui/material";
+import personAmountIcon from "../assets/room-personAmount-icon.svg";
+
+const RoomCard = ({
+  title,
+  showPeopleAmount = true,
+  image,
+  amount,
+  onClick,
+}) => {
+  return (
+    <Box
+      display={"flex"}
+      flexDirection={"column"}
+      justifyContent={"center"}
+      onClick={onClick}
+      sx={{
+        height: "100%",
+        width: "100%",
+        borderRadius: "13px",
+        backgroundImage: `url(${image})`,
+        backgroundSize: "cover",
+        "&:hover": {
+          cursor: "pointer",
+          boxShadow: 3,
+        },
+      }}
+    >
+      <Box
+        height={"50%"}
+        width={"100%"}
+        display={"flex"}
+        flexDirection={"column"}
+        justifyContent={"center"}
+        position={"relative"}
+        sx={{
+          background: "rgba(255, 255, 255, .3)",
+          backdropFilter: "blur(5px)",
+        }}
+      >
+        <Box alignSelf={"center"}>
+          <Typography
+            sx={{
+              color: "#fff",
+              fontWeight: "550",
+              fontSize: "calc(100% + 1rem)",
+            }}
+          >
+            {title}
+          </Typography>
+        </Box>
+
+        {showPeopleAmount && (
+          <Box
+            display={"flex"}
+            sx={{
+              position: "absolute",
+              bottom: "2px",
+              right: "10px",
+            }}
+          >
+            <Box>
+              <img src={personAmountIcon} height={"60%"} />
+            </Box>
+            <Box
+              sx={{
+                color: "#fff",
+                fontWeight: 550,
+                fontSize: "calc(100%)",
+                ml: 0.3,
+              }}
+            >
+              <Typography>{amount}</Typography>
+            </Box>
+          </Box>
+        )}
+      </Box>
+    </Box>
+  );
+};
+
+export default RoomCard;
--- client/src/pages/PublicRoomPage.jsx
+++ client/src/pages/PublicRoomPage.jsx
@@ -1,15 +1,11 @@
 import Page from "../containers/Page.jsx";
-import React from "react"
-import {Box} from "@mui/material";
+import React from "react";
+import { Box } from "@mui/material";
+import RoomCard from "../components/RoomCard.jsx";
+import backgroundRoom from "../assets/backgroundRoom.svg";
 
 const PublicRoomPage = () => {
-  return (
-    <Page title={"Public Room"}>
-      <Box sx={{color: "white"}}>
-        Public Room Page
-      </Box>
-    </Page>
-  )
-}
+  return <Page title={"Public Room"}></Page>;
+};
 
-export default PublicRoomPage
+export default PublicRoomPage;
