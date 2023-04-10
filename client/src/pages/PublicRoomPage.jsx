606c65b6cd29478c54dcba23b8d03d5c4e837415
FrankJi3 <frank.ji@centrapass.com>
2023-04-10 19:13:30 +1200

    feat: restore testing page

--- client/src/pages/PublicRoomPage.jsx
+++ client/src/pages/PublicRoomPage.jsx
@@ -1,20 +1,12 @@
 import Page from "../containers/Page.jsx";
 import React from "react";
 import { Box } from "@mui/material";
-import ModifiableTextField from "../components/ModifiableTextField.jsx";
 
 
 const PublicRoomPage = () => {
   return (
-    <Page title={"Public Room"} horizontalCenter verticalCenter>
+    <Page title={"Public Room"}>
       <Box sx={{ color: "white" }}>Public Room Page</Box>
-      <Box sx={{width: "250px", height: "60px"}}>
-        <ModifiableTextField
-          label={"USERNAME"}
-          value={"Frank Ji"}
-          onSubmitChange={(newValue) => alert(newValue)}
-        />
-      </Box>
     </Page>
   );
 };
