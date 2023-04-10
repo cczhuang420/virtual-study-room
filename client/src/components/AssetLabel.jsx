09bb71c8eca50e2208d7d07428ed465e2fd539bb
sma148 <sma148@aucklanduni.ac.nz>
2023-04-10 20:15:53 +1200

    format the layout of the asset label and added SVG files for 'xp' and 'money' icons

--- client/src/components/AssetLabel.jsx
+++ client/src/components/AssetLabel.jsx
@@ -1,10 +1,50 @@
-import { Box, Typography, Button } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import { Image } from "mui-image";
 import { useState } from "react";
 
-const AssetLabel = ({ name, image, xpValue, onClick }) => {
-    return (
-        <Box></Box>
-    )}
+{
+  /* best height and width:
+          width: "9%",
+          height: "5%",
+   */
+}
+
+const AssetLabel = ({ image, value }) => {
+  return (
+    <Box
+      display={"flex"}
+      flexDirection={"row"}
+      justifyContent={"space-evenly"}
+      alignItems={"center"}
+      sx={{
+        height: "100%",
+        width: "100%",
+        borderRadius: "20px",
+        background: "rgba(255, 255, 255, .5)",
+      }}
+    >
+      <Image
+        width={"30%"}
+        fit={"scale-down"}
+        height={"100%"}
+        src={image}
+        bgColor={""}
+        duration={0}
+      />
+      <Typography
+        sx={{
+          textAlign: "center",
+          fontWeight: "bold",
+          fontSize: "1.2em",
+          color: "white",
+          mr: "0.5rem",
+          overflow: "hidden",
+        }}
+      >
+        {value.toLocaleString()}
+      </Typography>
+    </Box>
+  );
+};
 
 export default AssetLabel;
