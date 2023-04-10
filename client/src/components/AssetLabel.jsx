e9c079c9c266e8a154bf6ff2a06d77ca516a018e
sma148 <sma148@aucklanduni.ac.nz>
2023-04-10 19:37:11 +1200

    add new AssetLabel component

---
+++ client/src/components/AssetLabel.jsx
@@ -0,0 +1,10 @@
+import { Box, Typography, Button } from "@mui/material";
+import { Image } from "mui-image";
+import { useState } from "react";
+
+const AssetLabel = ({ name, image, xpValue, onClick }) => {
+    return (
+        <Box></Box>
+    )}
+
+export default AssetLabel;
