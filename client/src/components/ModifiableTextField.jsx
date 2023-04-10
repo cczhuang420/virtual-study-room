2ba9956fd03e5f97a6de3173b37c925108335a50
FrankJi3 <frank.ji@centrapass.com>
2023-04-10 19:04:23 +1200

    feat: complete modify view

--- client/src/components/ModifiableTextField.jsx
+++ client/src/components/ModifiableTextField.jsx
@@ -1,4 +1,4 @@
-import {Box, IconButton, Typography, useTheme} from "@mui/material";
+import {Box, IconButton, TextField, Typography, useTheme} from "@mui/material";
 import {useState} from "react";
 import EditIcon from '@mui/icons-material/Edit';
 
@@ -20,19 +20,43 @@ const ModifiableTextField = ({label, value, onSubmitChange}) => {
         position: "relative"
       }}
     >
-      <Typography sx={{color: "white", fontSize: "20px"}}>
-        {value}
-      </Typography>
-      <Box sx={{position: "absolute", top: 4, left: 6, color: "white"}}>
-        <Typography>
-          {label}
-        </Typography>
-      </Box>
-      <Box sx={{position: "absolute", right: 2, top: "50%", transform: "translateY(-50%)"}}>
-        <IconButton>
-          <EditIcon sx={{color: "white"}} />
-        </IconButton>
-      </Box>
+      {!isModifying && (
+        <>
+          <Typography sx={{color: "white", fontSize: "20px"}}>
+            {value}
+          </Typography>
+          <Box sx={{position: "absolute", top: 4, left: 6, color: "white"}}>
+            <Typography>
+              {label}
+            </Typography>
+          </Box>
+          <Box sx={{position: "absolute", right: 2, top: "50%", transform: "translateY(-50%)"}}>
+            <IconButton onClick={() => setIsModifying(true)}>
+              <EditIcon sx={{color: "white"}} />
+            </IconButton>
+          </Box>
+        </>
+      )}
+      {isModifying && (
+        <TextField
+          autoFocus
+          // onBlur={() => setIsModifying(false)}
+          sx={{
+            height: "100%",
+            "& .MuiInputBase-root": {
+              height: "100%",
+            },
+            "& input": {
+              width: "100%",
+              height: "100%",
+              color: "white"
+            },
+            "& fieldset": {
+              border: "none"
+            }
+          }}
+        />
+      )}
     </Box>
   )
 }
