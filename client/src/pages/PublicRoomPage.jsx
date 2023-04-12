daca6e0bc69f43543e1d5b8074e98a4118269067
yinuoxue <yinuoxue117@163.com>
2023-04-12 20:12:22 +0800

    Delete testing code.

--- client/src/pages/PublicRoomPage.jsx
+++ client/src/pages/PublicRoomPage.jsx
@@ -1,8 +1,6 @@
 import Page from "../containers/Page.jsx";
 import React from "react";
 import { Box } from "@mui/material";
-import FriendCard from "../components/FriendCard.jsx";
-import profileImage from "../assets/temp-profile-image.svg"
 
 const PublicRoomPage = () => {
   return (
@@ -10,7 +8,6 @@ const PublicRoomPage = () => {
       <Box sx={{ color: "white" }} width={"100%"}>
         Public Room Page
       </Box>
-      <FriendCard profileImage={profileImage} name={"Mike"} />
     </Page>
   );
 };
