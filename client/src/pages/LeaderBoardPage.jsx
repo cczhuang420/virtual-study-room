b55a2baad3609e80f29c55839f6ab73f73158bf1
yinuoxue <yinuoxue117@163.com>
2023-04-12 18:17:52 +0800

    fix: tidy up the testing code.

--- client/src/pages/LeaderBoardPage.jsx
+++ client/src/pages/LeaderBoardPage.jsx
@@ -1,8 +1,6 @@
 import Page from "../containers/Page.jsx";
 import React from "react"
 import {Box} from "@mui/material";
-import profileImage from "../assets/profile-image.svg";
-import RankBar from "../components/RankBar.jsx";
 
 const LeaderboardPage = () => {
   return (
@@ -10,7 +8,6 @@ const LeaderboardPage = () => {
       <Box sx={{color: "white"}}>
         Leaderboard Page
       </Box>
-      <RankBar rankValue={"01"} profileImage={profileImage} name={"Mike Ma"} xpValue={"10,303"} assetValue={"3,600"} hours={"9999+"} />
     </Page>
   )
 }
