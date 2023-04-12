8161863765f4f59a941a3f03b1de5d6c13e929dd
cczhuang420 <zhuangxiaoxiao1988@gmail.com>
2023-04-12 23:30:52 +1200

    add ProductCard in Marketplace page

--- client/src/pages/MarketplacePage.jsx
+++ client/src/pages/MarketplacePage.jsx
@@ -1,13 +1,51 @@
 import Page from "../containers/Page.jsx";
 import React from "react"
-import {Box} from "@mui/material";
+import {Box, styled, Button} from "@mui/material";
+import ProductCard from "../components/ProductCard.jsx"
 
 const MarketplacePage = () => {
-  return (
+    const ColorButton = styled(Button)(() => ({
+        backgroundColor: "#9B84B4",
+        color: "black",
+        textTransform: "unset !important",
+        fontSize:20,
+        fontFamily:"Rubik",
+        '&:hover': {
+            opacity:"0.9",
+            backgroundColor: "#FFFFFF",
+            transform:"scale(1.02)",
+
+        },
+        '&:focus':{
+            backgroundColor: "#FFFFFF",
+            color:"black",
+        },
+        borderRadius:30,
+    }));
+
+    return (
     <Page title={"Marketplace"}>
-      <Box sx={{color: "white"}}>
-        Marketplace Page
-      </Box>
+        <>
+            <Box className="mb-10">
+                <Box className="flex flex-row space-x-5 m-10 mb-0">
+                    <ColorButton variant="contained" size="small" className="w-40">Background</ColorButton>
+                    <ColorButton variant="contained" size="small" className="w-40">Profile Photo</ColorButton>
+                </Box>
+
+                <Box className="flex flex-row flex-wrap space-x-10 space-y-10"
+                >
+                    <div></div>
+                    <ProductCard value="200"  productName="Notebook"/>
+                    <ProductCard value="200"  productName="Notebook"/>
+                    <ProductCard value="200"  productName="Notebook"/>
+                    <ProductCard value="200"  productName="Notebook"/>
+                    <ProductCard value="200"  productName="Notebook"/>
+                    <ProductCard value="200"  productName="Notebook"/>
+                </Box>
+            </Box>
+
+        </>s
+
     </Page>
   )
 }
