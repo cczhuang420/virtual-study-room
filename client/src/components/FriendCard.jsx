338115ca99e186fc062d694c0fb5096c05037c73
yinuoxue <yinuoxue117@163.com>
2023-04-13 20:34:02 +0800

    fix: modify the friend card layout.

--- client/src/components/FriendCard.jsx
+++ client/src/components/FriendCard.jsx
@@ -5,39 +5,55 @@ import { Image } from "mui-image";
 const FriendCard = ({ profileImage, name }) => {
   return (
     <Box
+      paddingLeft={2}
       display={"flex"}
       flexDirection={"row"}
-      justifyContent={"space-evenly"}
+      justifyContent={"center"}
       alignItems={"center"}
       sx={{
-        height: "10%",
-        width: "10%",
+        height: "100%",
+        width: "100%",
         border: 0,
         overflow: "hidden",
       }}
     >
-      <Image
-        width={"30%"}
-        height={"100%"}
-        fit={"scale-down"}
-        src={profileImage}
-        bgColor={""}
-        duration={0}
-      />
-      <Typography
+      <Box
         sx={{
-          textAlign: "center",
-          fontWeight: "medium",
-          fontSize: "1em",
-          color: "white",
-          mr: "0.5rem",
+          height: "100%",
+          width: "50%",
           overflow: "hidden",
         }}
       >
-        {name.toLocaleString()}
-      </Typography>
-    </Box>
-
+        <Image
+          width={"100%"}
+          height={"100%"}
+          fit={"scale-down"}
+          src={profileImage}
+          bgColor={""}
+          duration={0}
+        />
+      </Box>
+      <Box
+        sx={{
+          width: "100%",
+          overflow: "hidden",
+        }}
+      >
+        <Typography
+          sx={{
+            textAlign: "left",
+            fontWeight: "medium",
+            fontSize: "1em",
+            color: "white",
+            whiteSpace: "nowrap",
+            textOverflow: "ellipsis",
+            overflow: "hidden",
+          }}
+        >
+          {name.toLocaleString()}
+        </Typography>
+      </Box>
+    </Box >
   );
 };
 
