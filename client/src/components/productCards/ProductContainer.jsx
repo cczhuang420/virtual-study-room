d77919ea9ad8b69550ad2c8e7f6c2aa072e6f666
cczhuang420 <zhuangxiaoxiao1988@gmail.com>
2023-04-13 19:21:28 +1200

    add ProductContainer component

---
+++ client/src/components/productCards/ProductContainer.jsx
@@ -0,0 +1,35 @@
+import { Box } from "@mui/material";
+import BackgroundProductCard from "./BackgroundProductCard.jsx";
+import ProfileProductCard from "./ProfileProductCard.jsx";
+import MusicProductCard from "./MusicProductCard.jsx";
+import React from "react";
+
+const ProductContainer = ({ product }) => {
+  return (
+    <div>
+      {product === "background" ? (
+        <Box className="flex flex-row flex-wrap space-x-10 space-y-10">
+          <div></div>
+          <BackgroundProductCard value="200" productName="Notebook" />
+          <BackgroundProductCard value="200" productName="Notebook" />
+          <BackgroundProductCard value="200" productName="Notebook" />
+          <BackgroundProductCard value="200" productName="Notebook" />
+          <BackgroundProductCard value="200" productName="Notebook" />
+          <BackgroundProductCard value="200" productName="Notebook" />
+        </Box>
+      ) : product === "profile-photo" ? (
+        <Box className="flex flex-row flex-wrap space-x-10 space-y-10">
+          <div></div>
+          <ProfileProductCard value="200" productName="Profile Photo" />
+        </Box>
+      ) : (
+        <Box className="flex flex-row flex-wrap space-x-10 space-y-10">
+          <div></div>
+          <MusicProductCard value="200" productName="Music" />
+        </Box>
+      )}
+    </div>
+  );
+};
+
+export default ProductContainer;
