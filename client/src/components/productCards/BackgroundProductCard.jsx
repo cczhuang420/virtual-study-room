c3942db6eb081a85936fd7949e8488ee6c7277b2
cczhuang420 <zhuangxiaoxiao1988@gmail.com>
2023-04-13 19:24:53 +1200

    move purchase button to a created button component and update the BackgroundProduct card

--- client/src/components/productCards/BackgroundProductCard.jsx
+++ client/src/components/productCards/BackgroundProductCard.jsx
@@ -3,11 +3,10 @@ import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import BackgroundCard from "../../assets/background-card.svg";
 import MoneyIcon from "../../assets/asset-money-icon.svg";
-import { Box, styled } from "@mui/material";
+import { Box } from "@mui/material";
 import PurchaseButton from "../buttons/PurchaseButton.jsx";
 
 const BackgroundProductCard = ({ value, productName }) => {
