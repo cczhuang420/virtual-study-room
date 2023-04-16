d918440d88023955fcd4a6fa938d615a12c531e3
cczhuang420 <zhuangxiaoxiao1988@gmail.com>
2023-04-16 15:08:31 +1200

    remove ProductfilterButton

--- client/src/pages/MarketplacePage.jsx
+++ client/src/pages/MarketplacePage.jsx
@@ -1,14 +1,83 @@
 import Page from "../containers/Page.jsx";
 import React from "react";
 import { Box } from "@mui/material";
-import ProductFilterButton from "../components/buttons/products/ProductFilterButton.jsx";
+import { styled } from "@mui/system";
+import TabUnstyled, { tabUnstyledClasses } from "@mui/base/TabUnstyled";
+import { buttonUnstyledClasses } from "@mui/base/ButtonUnstyled";
+import TabsListUnstyled from "@mui/base/TabsListUnstyled";
+import { TabPanelUnstyled } from "@mui/base";
+import TabsUnstyled from "@mui/base/TabsUnstyled";
+import ProductContainer from "../components/productCards/ProductContainer.jsx";
 
 const MarketplacePage = () => {
+  const Tab = styled(TabUnstyled)`
+    font-family: "Rubik", "Nexa", "sans-serif";
+    color: black;
+    cursor: pointer;
+    font-size: 0.875rem;
+    font-weight: bold;
+    background-color: #9b84b4;
+    width: 120px;
+    padding: 8px 15px;
+    margin: 5px 6px 0 6px;
+    border: none;
+    border-radius: 50px;
+    display: flex;
+    justify-content: center;
+
+    &:hover {
+      background-color: #ccbce3;
+    }
+
+    &.${tabUnstyledClasses.selected} {
+      background-color: #fff;
+      color: black;
+    }
+
+    &.${buttonUnstyledClasses.disabled} {
+      opacity: 0.5;
+      cursor: not-allowed;
+    }
+  `;
+
+  const TabsList = styled(TabsListUnstyled)(
+    ({ theme }) => `
+  min-width: 400px;
+  margin-bottom: 2px;
+  margin-left:32px;
+  margin-top:30px;
+  display: flex;
+  background-color:transparent;
+  `
+  );
+
+  const TabPanel = styled(TabPanelUnstyled)`
+    width: 100%;
+    font-family: "Rubik", "Nexa", "sans-serif";
+    font-size: 0.875rem;
+  `;
   return (
     <Page title={"Marketplace"}>
       <>
         <Box className="mb-10 w-full">
-          <ProductFilterButton />
+          <div>
+            <TabsUnstyled defaultValue={0} selectionFollowsFocus>
+              <TabsList>
+                <Tab>Background</Tab>
+                <Tab>Profile Photo</Tab>
+                <Tab>Music</Tab>
+              </TabsList>
+              <TabPanel value={0}>
+                <ProductContainer value={0} />
+              </TabPanel>
+              <TabPanel value={1}>
+                <ProductContainer value={1} />
+              </TabPanel>
+              <TabPanel value={2}>
+                <ProductContainer value={2} />
+              </TabPanel>
+            </TabsUnstyled>
+          </div>
         </Box>
       </>
     </Page>
