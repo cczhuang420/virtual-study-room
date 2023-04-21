15a5ff4845fd8fbb9d4a4af118b99bcff86b9ff2
sma148 <sma148@aucklanduni.ac.nz>
2023-04-21 23:59:01 +1200

    complete first hald page components

--- client/src/components/RankBar.jsx
+++ client/src/components/RankBar.jsx
@@ -1,8 +1,14 @@
 import { Box, Typography } from "@mui/material";
 import { Image } from "mui-image";
 
-
-const RankBar = ({ rankValue, profileImage, name, xpValue, assetValue, hours }) => {
+const RankBar = ({
+  rankValue,
+  profileImage,
+  name,
+  xpValue,
+  assetValue,
+  hours,
+}) => {
   return (
     <Box
       display={"flex"}
@@ -10,7 +16,7 @@ const RankBar = ({ rankValue, profileImage, name, xpValue, assetValue, hours }) 
       justifyContent={"space-around"}
       alignItems={"center"}
       sx={{
-        height: "10%",
+        height: "100%",
         width: "100%",
         borderRadius: "10px",
         background: "rgba(255, 255, 255, .5)",
--- client/src/pages/LeaderBoardPage.jsx
+++ client/src/pages/LeaderBoardPage.jsx
@@ -7,6 +7,7 @@ import img2 from "../assets/background-card.svg";
 import ListItem from "@mui/material/ListItem";
 import FriendCard from "../components/FriendCard.jsx";
 import TopLeaderCard from "../components/TopLeaderCard.jsx";
+import RankBar from "../components/RankBar.jsx";
 
 const LeaderboardPage = () => {
   //fake data, will be replaced by the data which fetch from the backend
@@ -27,51 +28,114 @@ const LeaderboardPage = () => {
   const podiumArray = dataArray.slice(0, 3);
   const rankingArray = dataArray.slice(3);
 
+  const heightPercent = ["10%", "30%", "5%", "8%"];
   return (
     <Page title={"Leaderboard"}>
       <Box
-        padding={2}
-        margin={5}
+        margin={3}
         display={"flex"}
         flexDirection={"column"}
-        justifyContent={"center"}
-        alignItems={"flex-start"}
+        justifyContent={"flex-start"}
         sx={{
           width: "100%",
           height: "100%",
+          overflowY: "auto",
+          //background: "rgba(255, 255, 255, .5)",
         }}
       >
-        <Typography
-          marginLeft={3}
-          sx={{
-            textAlign: "flex-start",
-            fontSize: "3rem",
-            color: "white",
-            flexGrow: "0.6",
-          }}
-        >
-          Top Learners
-        </Typography>
         <Box
-          padding={2}
-          margin={5}
           display={"flex"}
           flexDirection={"row"}
           sx={{
             width: "100%",
-            height: "100%",
+            height: heightPercent[0],
+            //background: "rgba(255, 255, 255, .5)",
+          }}
+        >
+          <Typography
+            marginLeft={5}
+            sx={{
+              textAlign: "flex-start",
+              fontSize: "3rem",
+              color: "white",
+              flexGrow: "0.6",
+            }}
+          >
+            Top Learners
+          </Typography>
+        </Box>
+        <Box
+          display={"flex"}
+          flexDirection={"row"}
+          justifyContent={"space-around"}
+          alignItems={"space-between"}
+          sx={{
+            width: "100%",
+            height: heightPercent[1],
+            //background: "rgba(255, 255, 255, .5)",
           }}
         >
           {podiumArray.map(({ profileImage, name, ranking, hours }, index) => (
-            <ListItem key={index}>
+            <Box
+              display={"flex"}
+              flexDirection={"row"}
+              justifyContent={"space-around"}
+              sx={{
+                width: "100%",
+                height: "100%",
+                //background: "rgba(255, 255, 255, .5)",
+              }}
+              key={index}
+            >
               <TopLeaderCard
                 profileImage={profileImage}
                 name={name}
                 ranking={ranking}
                 hours={hours}
               />
-            </ListItem>
+            </Box>
           ))}
+        </Box>
+        <Box
+          display={"flex"}
+          flexDirection={"row"}
+          sx={{
+            width: "100%",
+            height: heightPercent[2],
+            //background: "rgba(255, 255, 255, .9)",
+          }}
+        >
+          <Typography
+            marginLeft={2}
+            sx={{
+              textAlign: "flex-start",
+              fontSize: "1.3rem",
+              color: "white",
+              flexGrow: "0.6",
+            }}
+          >
+            All Users
+          </Typography>
+        </Box>
+        <Box
+          display={"flex"}
+          marginLeft={1.7}
+          paddingBottom={1}
+          flexDirection={"row"}
+          sx={{
+            width: "92%",
+            height: heightPercent[3],
+            //background: "rgba(255, 255, 255, .5)",
+          }}
+        >
+          <RankBar
+            profileImage={""}
+            rankValue={"RANK"}
+            name={"NAME"}
+            xpValue={"XP"}
+            assetValue={"ASSET"}
+            hours={"HOURS"}
+          />
         </Box>
       </Box>
     </Page>
