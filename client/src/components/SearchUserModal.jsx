d914ce45b90bcb3184f127aa7dedd6d35ae94b98
qingyang0506 <1565134797qq@gmail.com>
2023-05-06 21:41:23 +1200

    fix the friend list issue

--- client/src/components/SearchUserModal.jsx
+++ client/src/components/SearchUserModal.jsx
@@ -1,22 +1,30 @@
-import React, {useState} from "react"
-import {Modal, Box, IconButton, TextField} from "@mui/material";
-import CloseIcon from "@mui/icons-material/Close"
-import {useMutation} from "../hooks/useMutation.js";
-import {HTTP_METHOD} from "../hooks/http-methods.js";
-import {useFetch} from "../hooks/useFetch.js";
+import React, { useState } from "react";
+import { Modal, Box, IconButton, TextField } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+import { useMutation } from "../hooks/useMutation.js";
+import { HTTP_METHOD } from "../hooks/http-methods.js";
+import { useFetch } from "../hooks/useFetch.js";
 import FriendList from "./FriendList.jsx";
-import {useAuth} from "../providers/AuthProvider.jsx";
-import {useNotification} from "../providers/NotificationProvider.jsx";
+import { useAuth } from "../providers/AuthProvider.jsx";
+import { useNotification } from "../providers/NotificationProvider.jsx";
 
-const SearchUserModal = ({open, onClose}) => {
-  const [searchText, setSearchText] = useState("")
-  const {isLoading, data} = useFetch("users")
-  const {getCustomUser} = useAuth()
-  const sendFriendRequestHandler = useMutation("friends/requests", HTTP_METHOD.POST)
-  const notify = useNotification()
+const SearchUserModal = ({ open, onClose }) => {
+  const [searchText, setSearchText] = useState("");
+  const { isLoading, data } = useFetch("users");
+  const { getCustomUser } = useAuth();
+  const sendFriendRequestHandler = useMutation(
+    "friends/requests",
+    HTTP_METHOD.POST
+  );
+  const notify = useNotification();
 
-  const allUsers = !isLoading ?
-    data.map(({_id, username, profile}) => ({id: _id, name: username, image: `/src/assets/profiles/${profile}`})) : []
+  const allUsers = !isLoading
+    ? data.map(({ _id, username, profile }) => ({
+        id: _id,
+        name: username,
+        image: `/src/assets/profiles/${profile}`,
+      }))
+    : [];
 
   return (
     <Modal
@@ -41,34 +49,34 @@ const SearchUserModal = ({open, onClose}) => {
           left: "50%",
           transform: "translate(-50%,-50%)",
           padding: 4,
-          borderRadius: 2
+          borderRadius: 2,
         }}
       >
         <Box sx={{ position: "absolute", top: 4, right: 4 }}>
           <IconButton onClick={onClose}>
-            <CloseIcon sx={{color: "white"}} />
+            <CloseIcon sx={{ color: "white" }} />
           </IconButton>
         </Box>
         <Box
           sx={{
             height: "100%",
             display: "flex",
-            flexDirection: "column"
+            flexDirection: "column",
           }}
         >
           <TextField
             value={searchText}
-            onChange={e => setSearchText(e.target.value)}
+            onChange={(e) => setSearchText(e.target.value)}
             placeholder={"Search users with username..."}
             sx={{
               backgroundColor: "rgba(255,255,255,.2)",
               borderRadius: "5px",
               "& fieldset": {
-                border: "none"
+                border: "none",
               },
               "& input": {
-                color: "white"
-              }
+                color: "white",
+              },
             }}
           />
           <Box
@@ -79,16 +87,21 @@ const SearchUserModal = ({open, onClose}) => {
             }}
           >
             <FriendList
-              friends={allUsers.filter(({name}) => name.includes(searchText))}
+              friends={allUsers.filter(
+                ({ name }) =>
+                  name != getCustomUser().username && name.includes(searchText)
+              )}
               onAddFriend={async (fid) => {
-                try{
-                  await sendFriendRequestHandler.run({query: {id: getCustomUser()._id, fid}})
-                  notify("Friend request sent")
+                try {
+                  await sendFriendRequestHandler.run({
+                    query: { id: getCustomUser()._id, fid },
+                  });
+                  notify("Friend request sent");
                 } catch (e) {
                   if (e.response.status === 409) {
-                    notify("Friend request sent")
+                    notify("Friend request sent");
                   } else {
-                    notify("Error, please try again")
+                    notify("Error, please try again");
                   }
                 }
               }}
@@ -97,7 +110,7 @@ const SearchUserModal = ({open, onClose}) => {
         </Box>
       </Box>
     </Modal>
-  )
-}
+  );
+};
 
-export default SearchUserModal
+export default SearchUserModal;
