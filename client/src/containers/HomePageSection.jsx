df1091590b7ef3418ad81585636093e37587c225
FrankJi3 <frank.ji@centrapass.com>
2023-05-09 11:06:00 +1200

    chore: remove background lottie

--- client/src/containers/HomePageSection.jsx
+++ client/src/containers/HomePageSection.jsx
@@ -6,20 +6,20 @@ import backgroundAnimation from "../assets/homepage-background.json"
 const HomePageSection = ({children, verticalCenter, horizontalCenter}) => {
   return (
     <Box sx={{minWidth: "100vw", minHeight: "100vh", display: "flex", overflowX: "hidden"}}>
-      <LottiePlayer
-        animationData={backgroundAnimation}
-        sx={{
-          position: "fixed",
-          top: 0,
-          bottom: 0,
-          left: 0,
-          right: 0,
-          display: {
-            xs: "none",
-            md: "block"
-          }
-        }}
-      />
+      {/*<LottiePlayer*/}
+      {/*  animationData={backgroundAnimation}*/}
+      {/*  sx={{*/}
+      {/*    position: "fixed",*/}
+      {/*    top: 0,*/}
+      {/*    bottom: 0,*/}
+      {/*    left: 0,*/}
+      {/*    right: 0,*/}
+      {/*    display: {*/}
+      {/*      xs: "none",*/}
+      {/*      md: "block"*/}
+      {/*    }*/}
+      {/*  }}*/}
+      {/*/>*/}
       <Box
         sx={{
           background: "linear-gradient(to right, #3f086a, #2e0659, #3f086a)",
