71d3905cac3316eba75244a48b38b4073866826c
sma148 <sma148@aucklanduni.ac.nz>
2023-05-11 13:16:25 +1200

    set up the backend testing infrastructure

---
+++ server/src/chats/__test__/chat.model.test.js
@@ -0,0 +1,5 @@
+import mongoose from 'mongoose';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import { ChatModel } from '../chat.model';
+
+it('is true', () => expect(true).toBeTruthy());
