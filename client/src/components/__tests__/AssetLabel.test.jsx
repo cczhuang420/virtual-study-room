384ab7cc4e16b78d9440b334c054ec18534ee34c
yinuoxue <yinuoxue117@163.com>
2023-05-11 23:19:41 +0800

    Set up the frontend testing infrastructure

---
+++ client/src/components/__tests__/AssetLabel.test.jsx
@@ -0,0 +1,4 @@
+import '@testing-library/jest-dom';
+import { describe, expect, it } from 'vitest';
+import { render, fireEvent, waitFor } from '@testing-library/react';
+import AssetLabel from '../AssetLabel';
