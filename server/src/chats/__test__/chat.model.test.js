9da9f2047089cd824575953d37a84196bcf18311
sma148 <sma148@aucklanduni.ac.nz>
2023-05-11 16:25:48 +1200

    fix test setup error

--- server/src/chats/__test__/chat.model.test.js
+++ server/src/chats/__test__/chat.model.test.js
@@ -2,7 +2,33 @@ import mongoose from 'mongoose';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import ChatModel from '../chat.model';
 import ChatController from '../chat.controller';
-import {chats} from '../../testdata'
+import {chats, users, friendRequests, privateRooms, products, publicRooms} from '../../testdata'
+
+let mongod;
+
+beforeAll(async () => {
+    mongod = await MongoMemoryServer.create();
+
+    const connectionString = mongod.getUri();
+    await mongoose.connect(connectionString, { useNewUrlParser: true });
+});
+
+beforeEach(async () => {
+
+    await mongoose.connection.db.dropDatabase();
+    const Chats = await mongoose.connection.db.createCollection('chats');
+    await Chats.insertMany(chats);
+});
+
+afterEach(async () => {
+    await mongoose.connection.db.dropCollection('chats')
+})
+
+afterAll(async () => {
+    await mongoose.disconnect();
+    await mongod.stop();
+});
 
 
 it('is true', () => expect(true).toBeTruthy());
+
