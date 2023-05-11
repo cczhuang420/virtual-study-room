11c04d851e2b21fa94203f077944bd6cf2fe0ee4
sma148 <sma148@aucklanduni.ac.nz>
2023-05-12 01:19:40 +1200

    set up user testing structure and created model test for users

---
+++ server/src/users/__test__/user.test.js
@@ -0,0 +1,56 @@
+import mongoose from 'mongoose';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import UserModel from '../user.model';
+import {chats, users, friendRequests, privateRooms, products, publicRooms} from '../../testdata'
+import router from '../user.router'
+import express from 'express';
+import request from 'supertest';
+
+let mongod;
+
+const app = express();
+app.use(express.json());
+app.use('/api/users', router);
+
+beforeAll(async () => {
+    mongod = await MongoMemoryServer.create();
+    const connectionString = mongod.getUri();
+    await mongoose.connect(connectionString, { useNewUrlParser: true });
+    await mongoose.connection.db.dropDatabase();
+    await mongoose.connection.db.dropDatabase();
+});
+
+beforeEach(async () => {
+    await mongoose.connection.db.dropDatabase();
+    await mongoose.connection.db.dropDatabase();
+    const Chats = await mongoose.connection.db.createCollection('chats');
+    const Users = await mongoose.connection.db.createCollection('users');
+    const FriendRequests = await mongoose.connection.db.createCollection('friendrequests');
+    const Products = await mongoose.connection.db.createCollection('products');
+    const PrivateRooms = await mongoose.connection.db.createCollection('privaterooms');
+    const PublicRooms = await mongoose.connection.db.createCollection('publicrooms');
+    await Chats.insertMany(chats);
+    await Users.insertMany(users);
+    await FriendRequests.insertMany(friendRequests);
+    await Products.insertMany(products);
+    await PrivateRooms.insertMany(privateRooms);
+    await PublicRooms.insertMany(publicRooms);
+});
+
+afterAll(async () => {
+    await mongoose.disconnect();
+    await mongod.stop();
+});
+
+
+describe('Model Tests', () => {
+    it('check user collection', async () => {
+        const dataFromDb = await UserModel.find({});
+        expect(dataFromDb).toBeTruthy();
+        expect(dataFromDb.length).toBe(2);
+
+        for (let i = 0; i < dataFromDb.length; i++) {
+            expect(dataFromDb[i]._id.toString()).toBe(chats[i]._id.toString());
+        }
+    });
+});
